292af7a7e0a83e29b1bee934949363bb67b79cd5
jokot <jokotriyanto12345@gmail.com>
2025-07-15 17:26:15 +0700

    Add initial project structure and platform-specific files for .NET MAUI application
    - Created Windows platform files including Package.appxmanifest and app.manifest for application identity and settings.
    - Added iOS platform files including AppDelegate.cs, Info.plist, Program.cs, and PrivacyInfo.xcprivacy for application configuration and privacy settings.
    - Included resource files such as app icons, fonts, images, and splash screen assets.
    - Established styles for UI components in Colors.xaml and Styles.xaml to maintain a consistent design across the application.
    - Configured launch settings for Windows development environment.

---
+++ MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Notes.Services;
+using Notes.ViewModels;
+
+namespace Notes;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		builder.Services.AddSingleton<NoteService>();
+		builder.Services.AddSingleton<AllNotesViewModel>();
+
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
